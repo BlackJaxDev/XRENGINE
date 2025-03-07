a065a84e0217f73076918d2f2b411db8c94ce0ea
BlackJaxDev <blackjax0@gmail.com>
2025-03-07 12:20:53 -0600

    Work on heightfields

--- XRENGINE/Scene/Physics/Physx/PhysxHeightField.cs
+++ XRENGINE/Scene/Physics/Physx/PhysxHeightField.cs
@@ -1,15 +1,18 @@
 ﻿using MagicPhysX;
+using System.Drawing.Imaging;
+using System.Drawing;
 using System.Numerics;
 using XREngine.Data;
 using static MagicPhysX.NativeMethods;
+using ImageMagick;
+using static XREngine.Rendering.Physics.Physx.IPhysicsGeometry;
 
 namespace XREngine.Rendering.Physics.Physx
 {
-    public unsafe class PhysxHeightField(PhysxScene scene, PxHeightField* heightFieldPtr) : PhysxBase
+    public unsafe class PhysxHeightField : PhysxBase
     {
-        public PxHeightField* HeightFieldPtr { get; } = heightFieldPtr;
+        public PxHeightField* HeightFieldPtr { get; }
         public override unsafe PxBase* BasePtr => (PxBase*)HeightFieldPtr;
-        public PhysxScene Scene { get; } = scene;
 
         public PxHeightFieldFlags Flags => HeightFieldPtr->GetFlags();
         public unsafe uint RowCount => HeightFieldPtr->GetNbRows();
@@ -17,6 +20,43 @@ namespace XREngine.Rendering.Physics.Physx
         public unsafe PxHeightFieldFormat Format => HeightFieldPtr->GetFormat();
         public unsafe uint SampleStride => HeightFieldPtr->GetSampleStride();
         public unsafe float ConvexEdgeThreshold => HeightFieldPtr->GetConvexEdgeThreshold();
+
+        public PhysxHeightField(PxHeightField* heightFieldPtr)
+        {
+            HeightFieldPtr = heightFieldPtr;
+        }
+        public PhysxHeightField(string imagePath)
+        {
+            MagickImage image = new(imagePath);
+            uint width = image.Width;
+            uint height = image.Height;
+            var samples = stackalloc PxHeightFieldSample[(int)(width * height)];
+
+            var values = image.GetPixels().GetValues() ?? throw new Exception("Image does not contain pixel values.");
+            if (values.Length != width * height)
+                throw new Exception("Image size does not match heightfield size.");
+
+            Parallel.For(0, values.Length, index =>
+            {
+                PxHeightFieldSample* sample = &samples[index];
+                sample->height = (short)values[index];
+                if (index % 2 != 0)
+                    sample->SetTessFlagMut();
+                else
+                    sample->ClearTessFlagMut();
+            });
+
+            PxHeightFieldDesc desc = PxHeightFieldDesc_new();
+            desc.nbColumns = width;
+            desc.nbRows = height;
+            desc.samples.data = samples;
+            desc.samples.stride = (uint)sizeof(PxHeightFieldSample);
+            desc.format = PxHeightFieldFormat.S16Tm;
+            //desc.convexEdgeThreshold = 3.0f;
+            //desc.flags = PxHeightFieldFlags.NoBoundaryEdges;
+
+            HeightFieldPtr = phys_PxCreateHeightField(&desc, PhysxScene.PhysicsPtr->GetPhysicsInsertionCallbackMut());
+        }
 
         public uint SaveCells(DataSource data)
             => HeightFieldPtr->SaveCells(data.Address, data.Length);
@@ -44,5 +84,20 @@ namespace XREngine.Rendering.Physics.Physx
 
         public unsafe uint GetTimestamp()
             => HeightFieldPtr->GetTimestamp();
+
+        public PxHeightFieldGeometry GetGeometry(
+            float heightScale = 1.0f,
+            float rowScale = 1.0f,
+            float columnScale = 1.0f,
+            bool tightBounds = false,
+            bool doubleSided = false)
+        {
+            PxMeshGeometryFlags flags = 0;
+            if (tightBounds)
+                flags |= PxMeshGeometryFlags.TightBounds;
+            if (doubleSided)
+                flags |= PxMeshGeometryFlags.DoubleSided;
+            return PxHeightFieldGeometry_new(HeightFieldPtr, flags, heightScale, rowScale, columnScale);
+        }
     }
 }
